1c2e8f0331d05f584e8fa0864fb921688a6ec96e
Clara Zammar <clarazammar4@gmail.com>
2023-01-28 16:41:49 +0200

    My first program is done

---
+++ MyFirstProgramJava.java
@@ -0,0 +1,8 @@
+public class MyFirstProgramJava {
+	public static void main(String[] args) {
+		System.out.print("This is my ");
+		System.out.println("first Java program");
+		System.out.print("Programming is fun");
+
+	}
+}
