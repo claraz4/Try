e2d7f6fd53364234bde63d0343c0d67ee933f24c
Clara Zammar <clarazammar4@gmail.com>
2023-01-28 17:01:24 +0200

    Year

--- Me.java
+++ Me.java
@@ -2,7 +2,7 @@ public class Me {
 	public static void main(String[] args) {
 		System.out.println("Clara Zammar");
 		System.out.println("Computer Engineering");
-	
+		System.out.println("First year");
 
 	}
 
